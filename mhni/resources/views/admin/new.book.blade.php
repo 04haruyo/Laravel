8be1f373f698a4f4e15a198c623c2446c89874bb
04iwamoto <haru-306@hotmail.co.jp>
2025-08-29 13:06:22 +0900

    first commit

---
+++ mhni/resources/views/admin/new.book.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>新規登録</title>
+</head>
+<body>
+    <h1>新規登録</h1>
+    <form action=""method="post">
+        @csrf
+        タイトル<input type="text" name="book_name" required><br>
+        著者<input type="text" name="author" required><br>
+        タイトル<input type="text" name="book_name" required><br>
+        タイトル<input type="text" name="book_name" required><br>
+        タイトル<input type="text" name="book_name" required><br>
+
+
+    </form>
+
+    
+</body>
+</html>
---
+++ mhni/resources/views/nomal/commentComplete.blade.php
@@ -0,0 +1,25 @@
+<?php
+    $evaluation=$_POST["evaluation"];
+    $comment=$_POST["comment"];
+    if($evaluation===""||$comment===""){
+        $result="おすすめ度の選択又はコメントが入力されていません";
+    
+    }else{
+        $result="コメントの投稿が完了しました"
+    }
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>コメント投稿完了画面</title>
+</head>
+<body>
+    <h1><?=$reuslt ?></h1>
+    <a href=""></a>
+    
+    
+</body>
+</html>
